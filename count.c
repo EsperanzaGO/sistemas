8dbf4a134c282a6052743b0a4688142e9ef21252
EsperanzaGO <95490448+EsperanzaGO@users.noreply.github.com>
2021-12-09 10:04:25 -0500

    start of the first program

---
+++ count.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int contador = 1;
+    pid_t rc;
+    rc = fork();
+	printf("Hola (pid:%d)\n", (int) getpid());
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }else if (rc == 0) {
+        printf("Hola, soy el hijo de  (pid:%d)\n", (int) getpid());
+        contador+= 5;
+    }else {
+        printf("Hola, soy el padre de %d (pid:%d)\n", rc, (int) getpid());
+        contador+= 2;
+    }
+    printf("%d\n", contador);
+    
+    return 0;
+}
