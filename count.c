f254beb59348a1de9a51af8c72de7ffc33f9d1aa
EsperanzaGO <95490448+EsperanzaGO@users.noreply.github.com>
2021-12-10 14:17:02 -0500

    Update count.c

--- count.c
+++ count.c
@@ -6,15 +6,15 @@ int main(int argc, char *argv[]) {
     int contador = 1;
     pid_t rc;
     rc = fork();
-	printf("Hola (pid:%d)\n", (int) getpid());
+	//printf("Hola (pid:%d)\n", (int) getpid());
     if (rc < 0) {
         fprintf(stderr, "fork failed\n");
         exit(1);
     }else if (rc == 0) {
-        printf("Hola, soy el hijo de  (pid:%d)\n", (int) getpid());
+       // printf("Hola, soy el hijo de  (pid:%d)\n", (int) getpid());
         contador+= 5;
     }else {
-        printf("Hola, soy el padre de %d (pid:%d)\n", rc, (int) getpid());
+        //printf("Hola, soy el padre de %d (pid:%d)\n", rc, (int) getpid());
         contador+= 2;
     }
     printf("%d\n", contador);
